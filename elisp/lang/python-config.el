adfd007372ccec83feb7c8f4e48710220de5aa83
trolleyyy <trolleyyy@gmail.com>
2016-05-29 23:03:32 +0200

    ipython windows fix

--- elisp/lang/python-config.el
+++ elisp/lang/python-config.el
@@ -10,12 +10,39 @@
 ;; python env
 (use-package python-environment)
 
-;(setq-default py-shell-name "ipython")
-;(setq-default py-which-bufname "IPython")
-;; use the wx backend, for both mayavi and matplotlib
-;(setq py-python-command-args
-;  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
-;(setq py-force-py-shell-name-p t)
+(setq
+ py-default-interpreter "ipython3"
+ python-shell-interpreter "ipython3"
+ py-which-bufname "IPython"
+ python-shell-interpreter-args "--colors=Linux --profile=default"
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ py-python-command-args "--gui=wx --pylab=wx -colors Linux"
+ py-ipython-command-args "")
+
+(defun python-ipython-filename-fix (orig-fun &rest args)
+  (message "Python called with args: %s" args)
+  (let ((in-path1 (nth 1 args))
+        (in-path2 (nth 5 args)))
+    (if in-path1
+        (setf (nth 1 args) (replace-regexp-in-string (regexp-quote "\\") (regexp-quote "/") in-path1)))
+    (if in-path2
+        (setf (nth 5 args) (replace-regexp-in-string (regexp-quote "\\") (regexp-quote "/") in-path2))))
+  (message "Python call replaced with args: %s" args)
+  (apply orig-fun args))
+
+(if (eq system-type 'windows-nt)
+    (progn
+      (custom-set-variables '(py-shell-name "ipython3"))
+      (custom-set-variables '(py-ipython-command-args ""))
+      (advice-add 'py-which-execute-file-command :around #'python-ipython-filename-fix )
+      (advice-add 'py--execute-file-base :around #'python-ipython-filename-fix)))
+; python-shell-completion-setup-code
+; "from IPython.core.completerlib import module_completion"
+; python-shell-completion-module-string-code
+; "';'.join(module_completion('''%s'''))\n"
+; python-shell-completion-string-code
+; "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 ;; switch to the interpreter after executing code
 ;(setq py-shell-switch-buffers-on-execute-p nil)
