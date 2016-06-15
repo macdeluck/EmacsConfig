18d4ab9700a4f16a4af4ad8f93b91d1dfc792827
trolleyyy <trolleyyy@gmail.com>
2016-06-15 12:04:10 +0200

    some more fci mode fixes

--- elisp/fci-mode-fix.el
+++ elisp/fci-mode-fix.el
@@ -1,18 +1,11 @@
-(defun sanityinc/fci-enabled-p () (symbol-value 'fci-mode))
-
 (defvar sanityinc/fci-mode-suppressed nil)
-(make-variable-buffer-local 'sanityinc/fci-mode-suppressed)
-
 (defadvice popup-create (before suppress-fci-mode activate)
-    "Suspend fci-mode while popups are visible"
-    (let ((fci-enabled (sanityinc/fci-enabled-p)))
-      (when fci-enabled
-        (setq sanityinc/fci-mode-suppressed fci-enabled)
-        (turn-off-fci-mode))))
-
+  "Suspend fci-mode while popups are visible"
+  (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-mode)
+  (when fci-mode
+    (turn-off-fci-mode)))
 (defadvice popup-delete (after restore-fci-mode activate)
-    "Restore fci-mode when all popups have closed"
-    (when (and sanityinc/fci-mode-suppressed
-               (null popup-instances))
-      (setq sanityinc/fci-mode-suppressed nil)
-      (turn-on-fci-mode)))
+  "Restore fci-mode when all popups have closed"
+  (when (and (not popup-instances) sanityinc/fci-mode-suppressed)
+    (setq sanityinc/fci-mode-suppressed nil)
+    (turn-on-fci-mode)))
